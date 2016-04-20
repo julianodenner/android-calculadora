46c4cbc094ca6a24d857b04a5720d6371bf90de6
jdenner <jdenner@outlook.com>
2016-04-20 09:55:37 -0300

    Update

--- app/src/main/java/com/jdenner/MainActivity.java
+++ app/src/main/java/com/jdenner/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_new);
+        setContentView(R.layout.activity_main);
         this.visor = (TextView) findViewById(R.id.visor);
         this.visorPrincipal = (TextView) findViewById(R.id.visorPrincipal);
         atualizarVisor();
